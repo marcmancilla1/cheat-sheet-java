e886db268bf8470a2ecd879685c5d860b43d11cb
Marc <148865740+MMP1010101@users.noreply.github.com>
2026-09-20 14:25:51 +0200

    switch

--- src/CondicionalChuleta.java
+++ src/CondicionalChuleta.java
@@ -1,4 +1,27 @@
 public class CondicionalChuleta {
 
-    
+  public static void main(String[] args) {
+
+    var day = 2;
+
+    switch (day) {
+      case 1:
+        System.out.println("Lunes");
+        break;
+      case 2:
+        System.out.println("Martes");
+        break;
+      default:
+        System.out.println("default");
+    }
+  }
+
+  /*
+  swich inpeccionando a distintas keis
+
+  if day == 1
+  if day == 2
+
+  que simplemente usar keii
+   */
 }
